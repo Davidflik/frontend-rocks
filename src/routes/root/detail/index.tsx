1ddd54f434aea772607b0cb2eb0eb5f18bbfc649
Davidflik <davidecaldarelli8@gmail.com>
2025-03-06 15:12:16 +0000

    url

--- src/routes/root/detail/index.tsx
+++ src/routes/root/detail/index.tsx
@@ -1,6 +1,13 @@
 export const DetailRoute=()=>{
    
     return (
-        
+        <div className="h-dvh flex flex-col items-center justify-center">
+        <div className="bg-white p-8 rounded-md shadow-lg">
+        <div className="flex flex-col items-center space-y-4">
+            <p>ciola</p>
+            </div>
+            </div>
+            </div>
+
     )
 }
--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -23,8 +23,8 @@ export const RootRoute = ()=>{
           </div>
   
           <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
-  
-          <div className="flex flex-col items-center space-y-4">
+  <div className="flex flex-col items-center space-y-4">
+          
             <button
               className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
               onClick={() => setCount((count) => count + 1)}
