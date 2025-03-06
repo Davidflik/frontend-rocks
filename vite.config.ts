1954aae75b8fcb5f12c71c527a8b18c5f791ad93
Davidflik <davidecaldarelli8@gmail.com>
2025-03-06 11:47:58 +0000

    url2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://github.com/Davidflik/frontend-rocks/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://davidflik.github.io/frontend-rocks/' : '/',
   plugins: [react(), tailwindcss()],
 })
