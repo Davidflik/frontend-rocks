28f6fad47a7fe2f5f234b73f12eabed6a8b38622
Davidflik <davidecaldarelli8@gmail.com>
2025-03-06 15:17:54 +0000

    url2

--- src/routes/root/detail/index.tsx
+++ src/routes/root/detail/index.tsx
@@ -4,7 +4,7 @@ export const DetailRoute=()=>{
         <div className="h-dvh flex flex-col items-center justify-center">
         <div className="bg-white p-8 rounded-md shadow-lg">
         <div className="flex flex-col items-center space-y-4">
-            <p>ciola</p>
+            <p>caronnese</p>
             </div>
             </div>
             </div>
